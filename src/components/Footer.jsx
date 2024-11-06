3b6d078b65ed5616002a051a017f13af82edf5b7
Kenlark <kenzokerachi@hotmail.fr>
2024-11-06 20:20:28 +0100

    Navbar v1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,11 @@
-const Footer = () => {};
+const Footer = () => {
+  return (
+    <>
+      <section className="footer">
+        <div></div>
+      </section>
+    </>
+  );
+};
 
 export default Footer;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,27 @@ import { NavLink } from "react-router-dom";
 
 import Logo from "../assets/images/4.png";
 import LogoPhone from "../assets/images/iconmonstr-phone-1.svg";
+import LogoMail from "../assets/images/iconmonstr-email-1.svg";
 
 const Navbar = () => {
   return (
     <>
+      <section className="info-number">
+        <div className="contact">
+          <p className="number flex-infos">
+            <img src={LogoPhone} alt="logo tel" className="phone" />
+            <span className="p-question">Une question ? :</span>
+            <a href="tel:+590690287436">(+590) 6 90 28 74 36</a>
+            <span className="slash"> / </span>
+          </p>
+          <p className="mail flex-mail">
+            <img src={LogoMail} alt="logo mail" className="mail-logo" />
+            <a href="mailto:resa.bamaloc971@gmail.com">
+              resa.bamaloc971@gmail.com
+            </a>
+          </p>
+        </div>
+      </section>
       <section className="navbar">
         <div className="flex-navbar">
           <div>
@@ -55,25 +72,11 @@ const Navbar = () => {
                   Infos Utiles
                 </NavLink>
               </li>
-              <li>
-                <NavLink
-                  to="contact"
-                  className={({ isActive }) =>
-                    isActive ? "active-btn" : "inactive-btn"
-                  }
-                >
-                  Contact
-                </NavLink>
-              </li>
             </ul>
           </nav>
-          <div className="contact">
-            <a href="tel:+590690287436">
-              <p>
-                <img src={LogoPhone} className="phone" /> +590 6 90 28 74 36
-              </p>
-            </a>
-          </div>
+          <NavLink to="contact">
+            <button className="btn-contact">Nous Contacter</button>
+          </NavLink>
         </div>
       </section>
     </>
