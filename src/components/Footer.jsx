f850eab459c75ab06ee8ed63120a81cc427cfd12
Kenlark <kenzokerachi@hotmail.fr>
2024-11-10 07:32:36 +0100

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,8 @@
 import Logo from "../assets/images/4.png";
 import Instagram from "../assets/images/iconmonstr-instagram-11-240.png";
 import Facebook from "../assets/images/iconmonstr-facebook-3-240.png";
+import Phone from "../assets/images/iconmonstr-email-2-240.png";
+import Mail from "../assets/images/iconmonstr-phone-2-240.png";
 
 const Footer = () => {
   return (
@@ -32,10 +34,27 @@ const Footer = () => {
             </div>
           </a>
         </div>
-        <div>
-          <h3>Nous Contacter</h3>
-          <p>Bama'Loc</p>
-          <p></p>
+        <div className="flex-logo-footer-2">
+          <a href="/contact" className="contact-footer">
+            <h3>Nous Contacter</h3>
+          </a>
+          <p className="name-enterprise">Bama'Loc</p>
+          <a href="tel:+590690287436">
+            <div className="a-footer-rs">
+              <img
+                src={Phone}
+                alt="logo telephone"
+                className="logo-footer-rs"
+              />
+              <span className="span-footer">(+590) 6 90 28 74 36</span>
+            </div>
+          </a>
+          <a href="mailto:resa.bamaloc971@gmail.com">
+            <div className="a-footer-rs">
+              <img src={Mail} alt="logo mail" className="logo-footer-rs" />
+              <span className="span-footer">resa.bamaloc971@gmail.com</span>
+            </div>
+          </a>
         </div>
       </section>
     </>
